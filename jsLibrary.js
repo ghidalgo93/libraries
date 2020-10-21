fbd449291f5b537c6715a8d9068b3cc696933519
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-10-21 11:17:08 -0600

    add removeAllChildNodes from DOM element fuction

--- jsLibrary.js
+++ jsLibrary.js
@@ -3,3 +3,11 @@
 round(value, decimals) {
 	return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
+
+// Removes all child nodes from a given DOM element
+// input: DOM element
+function removeAllChildNodes(parent){
+	while (parent.firstChild){
+		parent.removeChild(parent.firstChild);
+	}
+}
